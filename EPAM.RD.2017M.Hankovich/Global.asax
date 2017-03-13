968d71eb383d6e1154f8e492280a9b6bdc68777b
Sviataslau_Hankovich <Sviataslau_Hankovich@EPBYMINW3915>
2017-03-13 08:15:45 +0300

    Добавьте файлы проекта.

---
+++ EPAM.RD.2017M.Hankovich/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EPAM.RD._2017M.Hankovich.MvcApplication" Language="C#" %>
---
+++ EPAM.RD.2017M.Hankovich/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EPAM.RD._2017M.Hankovich
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
