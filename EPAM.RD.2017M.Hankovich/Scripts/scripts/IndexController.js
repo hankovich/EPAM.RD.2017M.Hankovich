2d18ebabc9b84162f7e4da590bbce7527c5eae4e
Sviataslau Hankovich <Sviataslau_Hankovich@epam.com>
2017-03-13 11:59:01 +0300

    small refactoring

--- EPAM.RD.2017M.Hankovich/Scripts/scripts/IndexController.js
+++ EPAM.RD.2017M.Hankovich/Scripts/scripts/IndexController.js
@@ -3,8 +3,7 @@
         $scope.books = [
             {
                 name: "1984",
-                src: "http://www.livelib.ru/reader/jennlawer/o/q2vh8epd/o-o.jpeg",
-                albumnName: '1' 
+                src: "http://www.livelib.ru/reader/jennlawer/o/q2vh8epd/o-o.jpeg"
             },
             {
                 name: "Holy Bible",
@@ -20,6 +19,35 @@
             }
         ];
 
+        $scope.albumns = [
+            {
+                albumnName: "First",
+                photos: [
+                {
+                    name: "1984",
+                    src: "http://www.livelib.ru/reader/jennlawer/o/q2vh8epd/o-o.jpeg"
+                },
+                {
+                    name: "Holy Bible",
+                    src: "https://pbs.twimg.com/profile_images/597441961814032384/x6jCddhT.jpg"
+                }
+                ]
+            },
+            {
+                albumnName: "Second",
+                photos: [
+                {
+                    name: "Harry Potter",
+                    src: "https://i.ytimg.com/vi/40egrwx44vw/maxresdefault.jpg"
+                },
+                {
+                    name: "TOUNIEJADTSYI",
+                    src: "http://www.nndb.com/people/022/000098725/alexander-lukashenko-1.jpg"
+                }
+                ]
+            }
+        ];
+
         $scope.remove = function(index){
             $scope.books.splice(index, 1);
         }
@@ -28,6 +56,8 @@
         {
             if (name != "" && src != "")
                 $scope.books.push({ name, src });
+            else
+                alert("name and src are required!");
         }
 
         $scope.user = {
