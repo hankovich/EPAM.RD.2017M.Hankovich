d7135f1ce3535932123ded4ba3e4f5ecf8f58865
hankovich <svyatoslav.hankovich@gmail.com>
2017-03-14 23:49:43 +0300

    Albums added

--- EPAM.RD.2017M.Hankovich/Scripts/scripts/IndexController.js
+++ EPAM.RD.2017M.Hankovich/Scripts/scripts/IndexController.js
@@ -1,23 +1,5 @@
 ﻿angular.module('books', [])
     .controller('IndexController', ['$scope', '$http', function ($scope, $http) {
-        $scope.books = [
-            {
-                name: "1984",
-                src: "http://www.livelib.ru/reader/jennlawer/o/q2vh8epd/o-o.jpeg"
-            },
-            {
-                name: "Holy Bible",
-                src: "https://pbs.twimg.com/profile_images/597441961814032384/x6jCddhT.jpg"
-            },
-            {
-                name: "Harry Potter",
-                src: "https://i.ytimg.com/vi/40egrwx44vw/maxresdefault.jpg"
-            },
-            {
-                name: "TOUNIEJADTSYI",
-                src: "http://www.nndb.com/people/022/000098725/alexander-lukashenko-1.jpg"
-            }
-        ];
 
         $scope.albumns = [
             {
@@ -48,20 +30,35 @@
             }
         ];
 
-        $scope.remove = function(index){
-            $scope.books.splice(index, 1);
+        $scope.remove = function(index, albumnName){
+            for (var i = 0; i < $scope.albumns.length; i++) {
+                if ($scope.albumns[i].albumnName === albumnName) {
+                    $scope.albumns[i].photos.splice(index, 1);
+                }
+            }
         }
 
-        $scope.add = function(name, src)
+        $scope.add = function(name, src, albumn)
         {
-            if (name != "" && src != "")
-                $scope.books.push({ name, src });
-            else
+            if (name !== "" && src !== "" && albumn !== "") {
+                var needSomeNewAlbumns = true;
+                for (var i = 0; i < $scope.albumns.length; i++) {
+                    if ($scope.albumns[i].albumnName === albumn) {
+                        $scope.albumns[i].photos.push({ name, src });
+                        needSomeNewAlbumns = false;
+                    }
+                }
+
+                if (needSomeNewAlbumns) {
+                    $scope.albumns.push({ albumnName: albumn, photos: [{name: name, src: src}]});
+                }
+            } else
                 alert("name and src are required!");
         }
 
         $scope.user = {
             name: "",
-            src: ""
+            src: "",
+            albumn: ""
         };
 }]);
